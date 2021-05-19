d15d8e62503c8d5ef62472aeebdfe90596b8170d
arifesq2 <arifesq2@gmail.com>
2021-05-19 23:16:22 +0530

    style: responsive header button changes.

--- styles/hikes/ihhead.js
+++ styles/hikes/ihhead.js
@@ -1,21 +1,25 @@
 import css from "styled-jsx/css";
 
-
 export const ihheaderStyles = css.global`
-	.navbar-expand-lg .navbar-nav .nav-link {
-		white-space: nowrap;
-		line-height: 17.5px;
-		text-align: left;
-		font-family: Poppins;
-		font-style: normal;
-		font-weight: bold;
-		font-size: 14px;
-		color: rgba(0,0,0,1);
-		padding-right: 2rem;
-		padding-left: 2rem;
-	}
+  .navbar-expand-lg .navbar-nav .nav-link {
+    white-space: nowrap;
+    line-height: 17.5px;
+    text-align: left;
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 14px;
+    color: rgba(0, 0, 0, 1);
+    padding-right: 2rem;
+    padding-left: 2rem;
+  }
 
-	.navbar-light .navbar-nav .nav-link.active, .navbar-light .navbar-nav .show > .nav-link {
-		border-bottom: 3px solid rgb(255, 193, 0);
-	}
+  .navbar-light .navbar-nav .nav-link.active,
+  .navbar-light .navbar-nav .show > .nav-link {
+    border-bottom: 3px solid rgb(255, 193, 0);
+  }
+
+  .navbar-light .navbar-toggler {
+    border: 0;
+  }
 `;
