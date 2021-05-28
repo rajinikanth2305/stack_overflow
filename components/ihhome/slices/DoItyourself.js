eed0595477fef7aa73474bfdc2fcb0f9e95a2379
arifesq2 <arifesq2@gmail.com>
2021-05-28 18:41:57 +0530

    feat: do it yourself carousel

--- components/ihhome/slices/DoItyourself.js
+++ components/ihhome/slices/DoItyourself.js
@@ -2,6 +2,9 @@ import React from "react";
 import { RichText } from "prismic-reactjs";
 import { doItStyles } from "styles";
 import Image from "next/image";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 /**
  * Founder Slice Components
  */
@@ -9,6 +12,39 @@ const DoItYourself = ({ slice }) => {
   const doitTitle = slice.primary.doit_title;
   const doitDec = slice.primary.doit_dec;
   const doit_doitImage_array = slice.items;
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
 
   const doitImage = doit_doitImage_array.map(function(data, i) {
     const doitimgs = {
@@ -19,7 +55,7 @@ const DoItYourself = ({ slice }) => {
 
     return (
       <>
-        <div className="col-lg-4 col-md-12" key={i}>
+        <div className="mx-2" key={i}>
           <div style={doitimgs} alt="imgs" className="doit_images"></div>
           <p className="doit_img_caption">{data.doit_image_caption[0].text}</p>
         </div>
@@ -42,10 +78,11 @@ const DoItYourself = ({ slice }) => {
             </div>
           </div>
           <div className="container container-custom">
-            <div className="row">
-              <div className="col-lg-12 col-md-12">
+            {/* <div className="col-lg-12 col-md-12">
                 <div className="row my-3">{doitImage}</div>
-              </div>
+              </div> */}
+            <div className="my-3">
+              <Slider {...settings}>{doitImage}</Slider>
             </div>
           </div>
         </div>
