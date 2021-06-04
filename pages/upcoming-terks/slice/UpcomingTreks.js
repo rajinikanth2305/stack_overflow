196efd3e04f6aef42aacdee810d9b54377957495
arifesq2 <arifesq2@gmail.com>
2021-06-04 23:00:36 +0530

    feat: upcoming treks section-1 ui development.

--- pages/upcoming-terks/slice/UpcomingTreks.js
+++ pages/upcoming-terks/slice/UpcomingTreks.js
@@ -15,12 +15,68 @@ const UpcomingTreks = ({ slice }) => {
               <h1 className="title-display-1">
                 {RichText.asText(upcomingTreksTitle)}
               </h1>
-              <h3 className="desc-dispaly-1">{RichText.asText(upcomingTreksDesc)}</h3>
+              <h3 className="desc-dispaly-1">
+                {RichText.asText(upcomingTreksDesc)}
+              </h3>
             </div>
             <div className="col-lg-6 col-md-6">
               <div className="card ih_card shadow-md">
-                <div className="card-body">
-                    <h5>Filter</h5>
+                <div className="card-body-padd">
+                  <div className="d-flex align-items-center">
+                    <div>
+                      <h3 className="title-diplay-3">Filter</h3>
+                    </div>
+                    <div className="mx-3">
+                      <p className="m-0 link_text">Clear all</p>
+                    </div>
+                  </div>
+                  <div className="row my-3">
+                    <div className="col-lg-6 col-md-12">
+                      <p className="form-label mb-1">
+                        When do you want to trek?
+                      </p>
+                      <select
+                        class="form-control mb-2"
+                        id="exampleFormControlSelect1"
+                      >
+                        <option>Season</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                      </select>
+                    </div>
+                    <div className="col-lg-6 col-md-12">
+                      <p className="form-label mb-1">Trek Difficulty</p>
+                      <select
+                        class="form-control mb-2"
+                        id="exampleFormControlSelect1"
+                      >
+                        <option>Difficulty</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-lg-6 col-md-12">
+                      <div className="row">
+                        <div className="col-lg-6 col-md-12">
+                          <p className="m-0 link_text">See All Treks</p>
+                        </div>
+                        <div className="col-lg-6 col-md-12">
+                          <p className="m-0 link_text">Family Treks ?</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-lg-6 col-md-12">
+                      <div className="text-right">
+                        <button className="btn btn-ih-green">FIND TREKS</button>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
--- styles/upcomingtrekspage/upcomingtrekstyle.js
+++ styles/upcomingtrekspage/upcomingtrekstyle.js
@@ -14,6 +14,10 @@ export const upcomingtrekpageStyle = css.global`
     padding: 40px 0;
   }
 
+  .card-body-padd {
+    padding: 17px 25px;
+  }
+
   .title-display-1 {
     text-align: left;
     font-family: Lora;
@@ -24,6 +28,16 @@ export const upcomingtrekpageStyle = css.global`
     text-transform: uppercase;
     border-bottom: 4px solid rgb(255, 193, 0);
     padding-bottom: 17px;
+  }
+
+  .title-diplay-3 {
+    line-height: 30px;
+    text-align: left;
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 24px;
+    color: rgba(0, 0, 0, 1);
   }
 
   .desc-dispaly-1 {
@@ -39,10 +53,66 @@ export const upcomingtrekpageStyle = css.global`
   }
 
   .ih_card {
-    border: 0.5px solid rgba(255, 193, 0, 1);
+    border: 1px solid rgb(255, 247, 223);
     border-radius: 0;
     filter: drop-shadow(0px 3px 6px rgba(0, 0, 0, 0.161));
     stroke: rgba(255, 193, 0, 1);
+  }
+
+  .link_text {
+    line-height: 18px;
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: lighter;
+    font-size: 14px;
+    color: rgba(112, 112, 112, 1);
+    text-decoration: underline;
+    cursor: pointer;
+  }
+
+  .form-control {
+    background-color: rgb(255, 193, 0);
+    border: 0;
+    border-radius: 0;
+    padding: 3px 5px;
+    font-weight: normal;
+    font-size: 16px;
+    color: rgba(0, 0, 0, 1);
+    font-family: Lora;
+    font-style: normal;
+  }
+
+  .form-control:focus {
+    background-color: rgb(255, 193, 0);
+    border: 0;
+    border-radius: 0;
+    padding: 3px 5px;
+    box-shadow: none;
+  }
+
+  .form-label {
+    line-height: 16px;
+    text-align: left;
+    font-family: Lora;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 12px;
+    color: rgba(112, 112, 112, 1);
+  }
+
+  .btn-ih-green {
+    background: rgb(91, 133, 70);
+    border: 0;
+    padding: 3px 30px;
+    border-radius: 0;
+    line-height: 24px;
+    text-align: center;
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    color: rgba(255, 255, 255, 1);
+    text-transform: uppercase;
   }
 
   @media only screen and (max-width: 660px) {
