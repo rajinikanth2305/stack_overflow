4da9d54dd00b62ebb5ee7a0c77720cdc09ab764c
arifesq2 <arifesq2@gmail.com>
2021-07-17 18:21:29 +0530

    feat: family trek screen section - 4 ui development.

--- components/familytrek/slices/index.js
+++ components/familytrek/slices/index.js
@@ -1,3 +1,4 @@
 export { default as FamilyTrekBanner } from "./FamilyTrekBanner";
 export { default as TrekExp } from "./TrekExp";
 export { default as MultiDayTrekComponent } from "./MultiDayTrekComponent";
+export { default as WeekendTrek } from "./WeekendTrek";
