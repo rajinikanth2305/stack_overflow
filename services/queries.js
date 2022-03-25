ff136058083fa5f07575b22039c20bc8077b6abf
deepthydon <todeepthydon@gmail.com>
2022-03-25 20:39:12 +0530

    refactor: optimized document search for prismic

--- services/queries.js
+++ services/queries.js
@@ -472,7 +472,7 @@ export const saveUserLocations =  async (bookingId,payload)  => {
 
 
 async function fetchDocs(page = 1, routes = []) {
-  const response = await Client().query('', { pageSize: 100, lang: '*', page });
+  const response = await Client().query('', { pageSize: 50, lang: '*', page });
   const allRoutes = routes.concat(response.results);
   if (response.results_size + routes.length < response.total_results_size) {
     return fetchDocs(page + 1, allRoutes);
