3484254b38e113982e2c8a4acd86e21d9a9696b2
arifmeeran <arifesq2@gmail.com>
2022-05-05 20:45:27 +0530

    fix: family trek - google form zoom issue in mobile view - fixed.

--- pages/family-trek/[uid].js
+++ pages/family-trek/[uid].js
@@ -36,7 +36,7 @@ const FamilyTrek = ({
           <meta http-equiv="X-UA-Compatible" content="IE=edge" />
           <meta
             name="viewport"
-            content="width=device-width, initial-scale=1.0"
+            content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=no;user-scalable=0;"
           />
           <title>{doc && pageTitle}</title>
         </Head>
