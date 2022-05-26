ed89d0cc3f2ae11e98ad950d001d5ffa588bb6d3
arifmeeran <arifesq2@gmail.com>
2022-05-26 06:11:30 +0530

    fix: open in new tab on upcoming treks page. - fixed.

--- components/upcoming/slice/BestTrekToDo.js
+++ components/upcoming/slice/BestTrekToDo.js
@@ -56,7 +56,7 @@ const BestTrekToDo = ({ slice, bestTrekToDoData }) => {
     }
   };
 
-  const trekToDoImage = bestTrekToDoData.map(function(data, i) {
+  const trekToDoImage = bestTrekToDoData.map(function (data, i) {
     const tData = data?.data?.body.find(x => x.slice_type === "trek_banner");
     let url;
     const slugUrl = data?.uid;
@@ -68,78 +68,80 @@ const BestTrekToDo = ({ slice, bestTrekToDoData }) => {
       <div key={i}>
         <div className="mx-4 m-mx-0 hvr-grow cursor-pointer">
           <Link href={url ? url : "#"}>
-            <div className="card_sec">
-              <div className="card trek_card">
-                <div className="uc_open_for_small_group_images">
-                  {tData?.primary?.trek_banner_image?.url && (
-                    <Image
-                      src={tData?.primary?.trek_banner_image?.url}
-                      layout="fill"
-                      objectFit="cover"
-                      objectPosition="50% 50%"
-                      alt="imgs"
-                    />
-                  )}
-                </div>
-                <div className="px-3 py-2">
-                  <div className="d-flex align-items-center card-info-text">
-                    <div>
-                      <p>{tData?.primary?.duration[0]?.text}</p>
-                    </div>
-                    <div>
-                      <p className="list-dot-style px-1">
-                        <span>.</span>
-                      </p>
-                    </div>
-                    <div>
-                      <p>{tData?.primary?.difficulty[0]?.text}</p>
-                    </div>
+            <a>
+              <div className="card_sec">
+                <div className="card trek_card">
+                  <div className="uc_open_for_small_group_images">
+                    {tData?.primary?.trek_banner_image?.url && (
+                      <Image
+                        src={tData?.primary?.trek_banner_image?.url}
+                        layout="fill"
+                        objectFit="cover"
+                        objectPosition="50% 50%"
+                        alt="imgs"
+                      />
+                    )}
                   </div>
+                  <div className="px-3 py-2">
+                    <div className="d-flex align-items-center card-info-text">
+                      <div>
+                        <p>{tData?.primary?.duration[0]?.text}</p>
+                      </div>
+                      <div>
+                        <p className="list-dot-style px-1">
+                          <span>.</span>
+                        </p>
+                      </div>
+                      <div>
+                        <p>{tData?.primary?.difficulty[0]?.text}</p>
+                      </div>
+                    </div>
 
-                  <div>
-                    <p className="title-diplay-3 text-uppercase">
-                      <b>
-                        {tData?.primary?.trek_caption?.length > 25
-                          ? `${tData?.primary?.trek_caption.substring(0, 25)}...`
-                          : tData?.primary?.trek_caption}
-                      </b>
-                    </p>
-                    <div className="p-display-2 trek_card_desc_min_height">
-                      {/* {RichText.asText(tData?.primary?.sub_heading)} */}
-                      {RichText.asText(tData?.primary?.sub_heading)
+                    <div>
+                      <p className="title-diplay-3 text-uppercase">
+                        <b>
+                          {tData?.primary?.trek_caption?.length > 25
+                            ? `${tData?.primary?.trek_caption.substring(0, 25)}...`
+                            : tData?.primary?.trek_caption}
+                        </b>
+                      </p>
+                      <div className="p-display-2 trek_card_desc_min_height">
+                        {/* {RichText.asText(tData?.primary?.sub_heading)} */}
+                        {RichText.asText(tData?.primary?.sub_heading)
                           ?.length > 75
                           ? `${RichText.asText(
                             tData?.primary?.sub_heading
-                            ).substring(0, 75)}...`
+                          ).substring(0, 75)}...`
                           : RichText.asText(tData?.primary?.sub_heading)}
-                    </div>
-                    <div className="d-flex align-items-center flex-wrap pt-2 pb-2 p-btn-btm">
-                      <div className="flex-grow-1">
-                        {getFamiltTrek !== undefined ? (
-                          <p className="m-0 fam_trek">
-                            <span>*</span> Family trek
-                          </p>
-                        ) : (
-                          ""
-                        )}
                       </div>
-                      <div>
-                        <button className="btn btn-ih-green">
-                          View Details
-                        </button>
+                      <div className="d-flex align-items-center flex-wrap pt-2 pb-2 p-btn-btm">
+                        <div className="flex-grow-1">
+                          {getFamiltTrek !== undefined ? (
+                            <p className="m-0 fam_trek">
+                              <span>*</span> Family trek
+                            </p>
+                          ) : (
+                            ""
+                          )}
+                        </div>
+                        <div>
+                          <button className="btn btn-ih-green">
+                            View Details
+                          </button>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
+            </a>
           </Link>
         </div>
       </div>
     );
   });
 
-  const trekToDoImageMobileView = bestTrekToDoData?.map(function(data, j) {
+  const trekToDoImageMobileView = bestTrekToDoData?.map(function (data, j) {
     const tData = data?.data?.body.find(x => x.slice_type === "trek_banner");
     let url;
     const slugUrl = data?.uid;
@@ -150,59 +152,59 @@ const BestTrekToDo = ({ slice, bestTrekToDoData }) => {
     return (
       <div className="col-6" key={j}>
         <Link href={url ? url : "#"}>
-        <div className="card_sec hvr-grow">
-          <div className="card trek_card_mb">
-            <div className="m-uc_open_for_small_group_images">
-              {tData?.primary?.trek_banner_image?.url && (
-                <Image
-                  src={tData?.primary?.trek_banner_image?.url}
-                  layout="fill"
-                  objectFit="cover"
-                  objectPosition="50% 50%"
-                  alt="imgs"
-                />
-              )}
-            </div>
-            <div className="px-3 py-2">
-              <div>
-                <h3 className="m-title-3 text-uppercase">
-                  {tData?.primary?.trek_caption?.length > 20
-                    ? `${tData?.primary?.trek_caption.substring(0, 20)}...`
-                    : tData?.primary?.trek_caption}
-                </h3>
-                <p className="m-display-2">
-                  {RichText.asText(tData?.primary?.sub_heading)}
-                </p>
-                <p className="m-card-info-text m-0">
-                  <span className="list-dot-style-mob"></span>{" "}
-                  {tData?.primary?.duration[0]?.text}
-                </p>
-                <p className="m-card-info-text">
-                  <span className="list-dot-style-mob"></span>{" "}
-                  {tData?.primary?.difficulty[0]?.text}
-                </p>
-                <div className="d-flex align-items-center flex-wrap pt-2 pb-2">
-                  <div className="flex-grow-1">
-                    {getFamiltTrek !== undefined ? (
-                      <p className="m-0 fam_trek">
-                        <span>*</span> Family trek
-                      </p>
-                    ) : (
-                      ""
-                    )}
-                  </div>
-                  <div className="p-abs-btn pb-3">
-                    {/* <Link href={url ? url : "#"}> */}
+          <div className="card_sec hvr-grow">
+            <div className="card trek_card_mb">
+              <div className="m-uc_open_for_small_group_images">
+                {tData?.primary?.trek_banner_image?.url && (
+                  <Image
+                    src={tData?.primary?.trek_banner_image?.url}
+                    layout="fill"
+                    objectFit="cover"
+                    objectPosition="50% 50%"
+                    alt="imgs"
+                  />
+                )}
+              </div>
+              <div className="px-3 py-2">
+                <div>
+                  <h3 className="m-title-3 text-uppercase">
+                    {tData?.primary?.trek_caption?.length > 20
+                      ? `${tData?.primary?.trek_caption.substring(0, 20)}...`
+                      : tData?.primary?.trek_caption}
+                  </h3>
+                  <p className="m-display-2">
+                    {RichText.asText(tData?.primary?.sub_heading)}
+                  </p>
+                  <p className="m-card-info-text m-0">
+                    <span className="list-dot-style-mob"></span>{" "}
+                    {tData?.primary?.duration[0]?.text}
+                  </p>
+                  <p className="m-card-info-text">
+                    <span className="list-dot-style-mob"></span>{" "}
+                    {tData?.primary?.difficulty[0]?.text}
+                  </p>
+                  <div className="d-flex align-items-center flex-wrap pt-2 pb-2">
+                    <div className="flex-grow-1">
+                      {getFamiltTrek !== undefined ? (
+                        <p className="m-0 fam_trek">
+                          <span>*</span> Family trek
+                        </p>
+                      ) : (
+                        ""
+                      )}
+                    </div>
+                    <div className="p-abs-btn pb-3">
+                      {/* <Link href={url ? url : "#"}> */}
                       <button className="btn m-btn-ih-green px-2">
                         View Dates / Register
                       </button>
-                    {/* </Link> */}
+                      {/* </Link> */}
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
         </Link>
       </div>
     );
