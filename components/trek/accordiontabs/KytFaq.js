4f545d9c4fab6ef990ada614b2883b3d39c18a7a
arifmeeran <arifesq2@gmail.com>
2022-06-04 12:25:19 +0530

    feat: trek faq section changes.

--- components/trek/accordiontabs/KytFaq.js
+++ components/trek/accordiontabs/KytFaq.js
@@ -1,7 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { RichText } from "prismic-reactjs";
 import Image from "next/image";
 import { experimentStyles } from "styles";
+import { useAccordionToggle } from 'react-bootstrap/AccordionToggle';
+import AccordionContext from 'react-bootstrap/AccordionContext';
+import { customStyles } from "styles";
+import Accordion from "react-bootstrap/Accordion";
+import Card from "react-bootstrap/Card";
 
 const KytFaq = ({ data }) => {
   const [faqDetails, setfaqDetails] = useState();
@@ -12,25 +17,66 @@ const KytFaq = ({ data }) => {
   }, []);
 
   async function findHowKytFaq() {
-    const slice = data && data.find(x => x.slice_type === "kyt_faq");
+    const slice = data && data.find(x => x.slice_type === "faq_about_trek");
     setfaqDetails(slice);
   }
 
-  const faqContent = faqDetails && faqDetails?.primary?.faq_section;
+  const faqContent = faqDetails && faqDetails?.primary?.heading1;
+  const faqArray = faqDetails && faqDetails?.items;
+  console.log(faqArray);
+
+  function ContextAwareToggle({ children, eventKey, callback }) {
+    const currentEventKey = useContext(AccordionContext);
+
+    const decoratedOnClick = useAccordionToggle(
+      eventKey,
+      () => callback && callback(eventKey),
+    );
+
+    const isCurrentEventKey = currentEventKey === eventKey;
+
+    return (
+      <button
+        type="button"
+        className={isCurrentEventKey ? 'show' : ''}
+        onClick={decoratedOnClick}
+      >
+        {children}
+      </button>
+    );
+  }
+
+  const faqArrayDetails = faqDetails && faqArray?.map(function(data, k) {
+    return (
+      <div className="col-md-12" key={k}>
+        <Card>
+          <Card.Header className="trek-faq-acc">
+            <ContextAwareToggle eventKey={k + 1}>{data?.question_heading[0]?.text}</ContextAwareToggle>
+          </Card.Header>
+          <Accordion.Collapse eventKey={k + 1}>
+            <Card.Body>
+              <div className="p-text-4 img-ctrl">
+                {RichText.render(data?.answer_content)}
+              </div>
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      </div>
+    );
+  });
+
+  const onTrekPageNavigate = bookingStatus => {
+    router.push(`/trek/${trekPageData.uid}`);
+  };
 
   return (
     <>
-      <div>
-        <div className="my-4 px-4">
-          <div className="p-text-1 text-capitalize mb-4">
-            {RichText.render(faqContent)}
-          </div>
-        </div>
-        <style jsx global>
-          {experimentStyles}
-        </style>
-      </div>
-    </>
+    <div className="mt-2">
+      <Accordion defaultActiveKey="0" className="reg-acc-tabs">
+        <div className="row">{faqArrayDetails}</div>
+      </Accordion>
+    </div>
+  </>
   );
 };
 export default KytFaq;
