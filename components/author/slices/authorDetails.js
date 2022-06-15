a73df39ab2d4c42b9ea63ff9ae64c8ba6ed83b90
arifmeeran <arifesq2@gmail.com>
2022-06-15 20:47:38 +0530

    feat: author articles data.

--- components/author/slices/authorDetails.js
+++ components/author/slices/authorDetails.js
@@ -14,6 +14,7 @@ const AuthorDetails = ({ slice }) => {
     const { query } = useRouter();
     const authorName = query && query?.name;
     const [authorData, setAuthorData] = useState();
+    const [authorArticles, setAuthorArticles] = useState();
 
     useEffect(() => {
         if (!authorName) {
@@ -31,16 +32,69 @@ const AuthorDetails = ({ slice }) => {
         };
         const getAuthorArticles = async () => {
             const client = Client();
-            const authorArticles = await client.query(
-                Prismic.Predicates.fulltext(
-                    "my.post.author_link",
-                    authorName
-                )
+            // const authorArticles = await client.query(
+            //     Prismic.Predicates.at("my.Post.author_link", authorName)
+            // );
+            const authorArticles = await client.query([
+                Prismic.Predicates.at("document.type", "post")], {
+                pageSize: 250,
+            }
             );
+            setAuthorArticles(authorArticles);
         };
         getAuthorDetails();
         getAuthorArticles();
     }, [authorName]);
+
+    const articlesByAthorName = authorArticles && authorArticles?.results?.filter(x => x?.data?.author_link?.uid === authorName);
+    const auth = authorName.replace(/-/g, " ");
+
+    const articlesByAthorNameSection = articlesByAthorName && articlesByAthorName?.map(function (data, i) {
+        const authorName = data?.data?.author_link?.uid.replace(/-/g, " ");
+        let url;
+        const slugUrl = data?.uid;
+        if (slugUrl) {
+            url = `/blog/${slugUrl}`;
+        }
+        const getArticleImage = data?.data?.body?.find(
+            x => x.slice_type === "feature_image"
+        );
+        const getArticleHeadingText = data?.data?.body?.find(
+            x => x.slice_type === "text"
+        );
+        return (
+            <div key={i} className="col-lg-4 col-md-6">
+                <a href={url ? url : "#"}>
+                    <div className="d-flex align-items-center row mb-4 cursor-pointer">
+                        <div className="col-3 col-lg-3 col-md-12">
+                            {getArticleImage?.primary?.feature_image?.url ? (
+                                <img
+                                    src={getArticleImage?.primary?.feature_image?.url}
+                                    alt="articleImage"
+                                    className="diyres_img_bg_img"
+                                />
+                            ) : (
+                                <img src="../ip.png" className="diyres_img_bg_img" />
+                            )}
+                        </div>
+                        <div className="col-9 col-lg-9 col-md-12">
+                            <p className="p-text-3">
+                                <b>{RichText.asText(data?.data?.title)}</b>
+                            </p>
+                            <div>
+                                <p className="p-text-small m-0 text-capitalize">
+                                    <em>By {authorName}</em>
+                                </p>
+                                <p className="p-text-small m-0 pt-0">
+                                    <em>{data?.data?.date}</em>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        );
+    });
 
     return (
         <>
@@ -71,6 +125,13 @@ const AuthorDetails = ({ slice }) => {
                             </div>
                         </div>
                         <div className="col-md-2" />
+                    </div>
+
+                    <div className="row my-4">
+                        <h2 className="title-h3 mb-5">
+                            <span className="border-bottom-custom"><b>Article by {authorName && auth}</b></span>
+                        </h2>
+                        {articlesByAthorNameSection}
                     </div>
                 </div>
                 <style jsx global>
