20dd77530706bbd1453c8215033adaf76163d322
ebrahimbookwala <ebrahimbookwala@hotmail.com>
2022-09-13 20:44:53 +0530

    Made a file and a few changes

--- pages/_document.js
+++ pages/_document.js
@@ -20,10 +20,10 @@ class MyDocument extends Document {
         <Head>
           <meta charset="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta
+          {/* <meta
             name="viewport"
             content="width=device-width, initial-scale=1.0"
-          />
+          /> */}
           <link
             href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900"
             rel="stylesheet"
