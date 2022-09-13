50eeb0360a45604c54b1ab4cffe907b08fad1141
ebrahimbookwala <ebrahimbookwala@hotmail.com>
2022-09-13 19:24:24 +0530

    Fixed a few console errors

--- pages/_document.js
+++ pages/_document.js
@@ -19,7 +19,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta charset="utf-8" />
-          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1.0"
