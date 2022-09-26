6f4845e927feb6d147014e4b79a4a1b945a156bf
Kabir <kabir4691@gmail.com>
2022-09-26 14:36:02 +0530

    Quick fix for webinar popup issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 import React, { useEffect, useState } from "react";
+import { useRouter } from 'next/router';
 import { HIDE_POPUP_LS_KEY } from "../utils/constants";
 import { EntryPopup } from "slices/entryPopup";
 
 function MyApp({ Component, pageProps }) {
+
+  const router = useRouter();
 
   const [showPopup, setShowPopup] = useState(false);
 
@@ -13,7 +16,15 @@ function MyApp({ Component, pageProps }) {
     setShowPopup(false);
   }
 
+  const onConfirm = () => {
+    localStorage.setItem(HIDE_POPUP_LS_KEY, "true");
+  }
+
   useEffect(() => {
+    if (router != null && router.pathname != null && router.pathname.startsWith("/webinar-thank-you")) {
+      setShowPopup(false);
+    }
+
     setTimeout(() => {
       setShowPopup(localStorage.getItem(HIDE_POPUP_LS_KEY) == null);
     }, 3000);
@@ -22,7 +33,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-      {showPopup && <EntryPopup onCancel={hidePopup} onConfirm={hidePopup} />}
+      {showPopup && <EntryPopup onCancel={hidePopup} onConfirm={onConfirm} />}
     </>
   );
 }
