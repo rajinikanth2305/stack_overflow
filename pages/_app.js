7e5ecef7df30226445dfa3e82bc1071326e94e59
Kabir <kabir4691@gmail.com>
2022-10-09 21:13:39 +0530

    Revert "Remove webinar popup"
    This reverts commit d3235af22f6d7ca22e88c7ed94ea6b9a046ba7d5.

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,39 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useRouter } from 'next/router';
+import { HIDE_POPUP_LS_KEY } from "../utils/constants";
+import { EntryPopup } from "slices/entryPopup";
 
 function MyApp({ Component, pageProps }) {
+
+  const router = useRouter();
+
+  const [showPopup, setShowPopup] = useState(false);
+
+  const hidePopup = () => {
+    localStorage.setItem(HIDE_POPUP_LS_KEY, "true");
+    setShowPopup(false);
+  }
+
+  const onConfirm = () => {
+    localStorage.setItem(HIDE_POPUP_LS_KEY, "true");
+  }
+
+  useEffect(() => {
+    if (router != null && router.pathname != null && router.pathname.startsWith("/webinar-thank-you")) {
+      setShowPopup(false);
+    }
+
+    setTimeout(() => {
+      setShowPopup(localStorage.getItem(HIDE_POPUP_LS_KEY) == null);
+    }, 3000);
+  }, [])
 
   return (
     <>
       <Component {...pageProps} />
+      {showPopup && <EntryPopup onCancel={hidePopup} onConfirm={onConfirm} />}
     </>
   );
 }
