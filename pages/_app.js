dcfcd73f9cc8124e0579fbe542080a91d8a6276b
Kabir <kabir4691@gmail.com>
2022-10-29 11:30:32 +0530

    Remove webinar popup

--- pages/_app.js
+++ pages/_app.js
@@ -6,35 +6,9 @@ import { linkResolver, repositoryName } from '../prismicio'
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 // import "../styles/buttonHoverStyle.css"
-import React, { useEffect, useState } from "react";
-import { useRouter } from 'next/router';
-import { HIDE_POPUP_LS_KEY } from "../utils/constants";
-import { EntryPopup } from "slices/entryPopup";
+import React from "react";
 
 function MyApp({ Component, pageProps }) {
-
-  const router = useRouter();
-
-  const [showPopup, setShowPopup] = useState(false);
-
-  const hidePopup = () => {
-    localStorage.setItem(HIDE_POPUP_LS_KEY, "true");
-    setShowPopup(false);
-  }
-
-  const onConfirm = () => {
-    localStorage.setItem(HIDE_POPUP_LS_KEY, "true");
-  }
-
-  useEffect(() => {
-    if (router != null && router.pathname != null && router.pathname.startsWith("/webinar-thank-you")) {
-      setShowPopup(false);
-    }
-
-    setTimeout(() => {
-      setShowPopup(localStorage.getItem(HIDE_POPUP_LS_KEY) == null);
-    }, 3000);
-  }, [])
 
   return <PrismicProvider
     linkResolver={linkResolver}
@@ -46,7 +20,6 @@ function MyApp({ Component, pageProps }) {
   >
     <PrismicPreview repositoryName={repositoryName}>
       <Component {...pageProps} />
-      {showPopup && <EntryPopup onCancel={hidePopup} onConfirm={onConfirm} />}
     </PrismicPreview>
   </PrismicProvider>
 }
